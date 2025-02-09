95badda4bcad5e84016662367d8fa3e7dec4b0ee
Guilherme Lacerda <lacerdagui42@gmail.com>
2025-02-09 20:10:16 -0300

    Criado Product Controller e metodo saveProduct

---
+++ src/main/java/com/guilhermels/crud/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.guilhermels.crud.controllers;
+
+import com.guilhermels.crud.dtos.ProductRequestDto;
+import com.guilhermels.crud.dtos.ProductResponseDto;
+import com.guilhermels.crud.entities.ProductEntity;
+import com.guilhermels.crud.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> saveProduct(@RequestBody ProductRequestDto productRequestDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.saveProduct(productRequestDto));
+    }
+
+}
