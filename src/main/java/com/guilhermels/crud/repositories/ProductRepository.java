77b2ad98c28302ba99dadc2cb5efd2cac00683f3
Guilherme Lacerda <lacerdagui42@gmail.com>
2025-02-09 20:11:04 -0300

    Criado ProductRepository base

---
+++ src/main/java/com/guilhermels/crud/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.guilhermels.crud.repositories;
+
+import com.guilhermels.crud.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
+}
