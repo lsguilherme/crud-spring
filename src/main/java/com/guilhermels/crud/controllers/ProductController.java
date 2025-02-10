5cc6c98e4d60e26d9de1d163f9d73e71b458f7a5
Guilherme Lacerda <lacerdagui42@gmail.com>
2025-02-10 02:06:28 -0300

    Criar paginação

--- src/main/java/com/guilhermels/crud/controllers/ProductController.java
+++ src/main/java/com/guilhermels/crud/controllers/ProductController.java
@@ -3,9 +3,12 @@ package com.guilhermels.crud.controllers;
 import com.guilhermels.crud.dtos.requests.ProductRequestDto;
 import com.guilhermels.crud.dtos.responses.ProductResponseDto;
 import com.guilhermels.crud.services.ProductService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "products")
@@ -18,18 +21,33 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductResponseDto> saveProduct(@RequestBody ProductRequestDto productRequestDto){
+    public ResponseEntity<ProductResponseDto> saveProduct(@RequestBody ProductRequestDto productRequestDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.saveProduct(productRequestDto));
     }
 
     @GetMapping(value = "{id}")
-    public ResponseEntity<ProductResponseDto> findProductById(@PathVariable Integer id){
+    public ResponseEntity<ProductResponseDto> findProductById(@PathVariable Integer id) {
         return ResponseEntity.status(HttpStatus.OK).body(productService.findProductById(id));
     }
 
     @DeleteMapping(value = "{id}")
-    public ResponseEntity<String> deleteProduct(@PathVariable Integer id){
+    public ResponseEntity<String> deleteProduct(@PathVariable Integer id) {
         productService.deleteProductById(id);
         return ResponseEntity.ok("Item deletado com sucesso");
+    }
+
+    @PutMapping(value = "{id}")
+    public ResponseEntity<ProductResponseDto> updateProductById(@PathVariable(value = "id") Integer id,
+                                                                @RequestBody ProductRequestDto productRequestDto) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(productService.updateProductById(id, productRequestDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductResponseDto>> findAllProducts(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size
+            ) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.findAllProducts(page, size));
     }
 }
--- src/main/java/com/guilhermels/crud/services/ProductService.java
+++ src/main/java/com/guilhermels/crud/services/ProductService.java
@@ -4,7 +4,13 @@ import com.guilhermels.crud.dtos.requests.ProductRequestDto;
 import com.guilhermels.crud.dtos.responses.ProductResponseDto;
 import com.guilhermels.crud.entities.ProductEntity;
 import com.guilhermels.crud.repositories.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -16,32 +22,35 @@ public class ProductService {
     }
 
 
-    public ProductResponseDto saveProduct(ProductRequestDto productRequestDto){
+    public ProductResponseDto saveProduct(ProductRequestDto productRequestDto) {
         ProductEntity productEntity = new ProductEntity(productRequestDto.name(), productRequestDto.price(), productRequestDto.description());
         productEntity = productRepository.save(productEntity);
-        return new ProductResponseDto(
-                productEntity.getId(),
-                productEntity.getName(),
-                productEntity.getPrice(),
-                productEntity.getDescription(),
-                productEntity.getCreatedAt(),
-                productEntity.getUpdatedAt()
-        );
+        return productEntity.toDto();
     }
 
-    public ProductResponseDto findProductById(Integer id){
+    public ProductResponseDto findProductById(Integer id) {
         ProductEntity productEntity = productRepository.findById(id).orElseThrow();
-        return new ProductResponseDto(
-                productEntity.getId(),
-                productEntity.getName(),
-                productEntity.getPrice(),
-                productEntity.getDescription(),
-                productEntity.getCreatedAt(),
-                productEntity.getUpdatedAt()
-        );
+        return productEntity.toDto();
     }
 
-    public void deleteProductById(Integer id){
+    public void deleteProductById(Integer id) {
         productRepository.deleteById(id);
+    }
+
+    public ProductResponseDto updateProductById(Integer id, ProductRequestDto productRequestDto) {
+        ProductEntity productEntity = productRepository.findById(id).get();
+
+        productEntity.setName(productRequestDto.name());
+        productEntity.setPrice(productRequestDto.price());
+        productEntity.setDescription(productRequestDto.description());
+
+        productEntity = productRepository.save(productEntity);
+        return productEntity.toDto();
+    }
+
+    public Page<ProductResponseDto> findAllProducts(int page, int size) {
+        Page<ProductEntity> productEntities = productRepository.findAll(PageRequest.of(page, size));
+
+        return productEntities.map(ProductEntity::toDto);
     }
 }
