de62a519559fdab11745ad90e6fbc13b5f87549b
Guilherme Lacerda <lacerdagui42@gmail.com>
2025-02-20 00:26:35 -0300

    Instalar Mapper e criar ProductMapper

---
+++ src/main/java/com/guilhermels/crud/mappers/ProductMapper.java
@@ -0,0 +1,18 @@
+package com.guilhermels.crud.mappers;
+
+import com.guilhermels.crud.dtos.requests.ProductRequestDto;
+import com.guilhermels.crud.dtos.responses.ProductResponseDto;
+import com.guilhermels.crud.entities.ProductEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ProductMapper {
+
+    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+
+    ProductEntity toEntity(ProductResponseDto productResponseDto);
+
+    ProductRequestDto toDto(ProductEntity productEntity);
+
+}
